4dca87f8e9d50f04f74f4cbe8a5a8d21894882ef
Jusong Yu <jusong.yeu@gmail.com>
2022-11-06 04:59:21 +0100

    WIP has issue with {WorkflowNode}

--- src/aiida_core_i18n/__init__.py
+++ src/aiida_core_i18n/__init__.py
@@ -1,4 +1,9 @@
 import re
+import typing
+import deepl
+import os
+
+DEEPL_TOKEN = os.environ.get("DEEPL_TOKEN")
 
 def str_pp(raw_str):
     """deepl zh_CN has problem when translate the `` in CJK from English,
@@ -18,3 +23,56 @@ def str_pp(raw_str):
     res = re.sub(r'(?:(?:(?<!^)(?<!\s)(?<!`))(``\w.*?``))', r' \1', add_space, flags=re.ASCII)
     
     return res.strip()
+
+def translate(inp_str, target_lang="ZH"):
+    """Call deepl API to tranlate and do post process"""
+    translator = deepl.Translator(DEEPL_TOKEN)
+    
+    try:
+        translated = translator.translate_text(
+            inp_str, 
+            source_lang="EN", 
+            target_lang=target_lang,
+            # formality="more", # not supported by ZH
+        )
+    except deepl.DeepLException as exc:
+        return ""
+    else:
+        res = str_pp(translated.text)
+        
+        return res
+
+def po_translate(lines: typing.List[str]):
+    output_lines = [i for i in lines]
+    for ln, line in enumerate(lines):
+        if line.startswith("msgid "):
+            ln_start = ln
+            for count, inner_line in enumerate(lines[ln:]):
+                if inner_line.startswith("msgstr "):
+                    ln_end = ln_start + count
+                    break
+                
+            # print(f"start: {ln_start}: {lines[ln_start]}")
+            # print(f"end: {ln_end}: {lines[ln_end]}")
+            
+            # if translated, skip， otherwise will override the translated result
+            if lines[ln_end] != 'msgstr ""':
+                continue
+            
+            if ln_end - ln_start > 1:
+                inp_str = "".join([i.strip('"') for i in lines[ln_start+1:ln_end]])
+            else:
+                # get the string from double quotes
+                inp_str = line.removeprefix('msgid "').removesuffix('"')
+                
+            # Do nothing to empty str
+            if inp_str == "":
+                continue
+            
+            translated = translate(inp_str)
+            output_lines[ln_end] = f'msgstr "{translated}"'
+            
+    print('\n'.join(output_lines))
+    return output_lines
+    
+            
--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aiida_core_i18n import str_pp
+from aiida_core_i18n import str_pp, po_translate
 
 @pytest.mark.parametrize(
     ('input', 'expected'),
@@ -9,10 +9,57 @@ from aiida_core_i18n import str_pp
         ("`AiidaApi', 继承`flask_restful.Api'。该类定义了由REST API提供的资源。", "``AiidaApi``, 继承 ``flask_restful.Api``。该类定义了由REST API提供的资源。"),
         ("`AiidaApi`，继承`flask_restful.Api`。该类定义了由REST API提供的资源。", "`AiidaApi`，继承 `flask_restful.Api`。该类定义了由REST API提供的资源。"),
         ("所有的Python内置类型都可以被Flask序列化（例如``int``、``float``、``str``等），而对于自定义类型的序列化，我们让你参考`Flask文档<http://flask.pocoo.org/docs/>`_。", "所有的Python内置类型都可以被Flask序列化（例如 ``int``、 ``float``、 ``str``等），而对于自定义类型的序列化，我们让你参考 `Flask文档<http://flask.pocoo.org/docs/>`_。"),
-        ("参见 :py:mod:`aiida.plugins`中的API文档。", "参见 :py:mod:`aiida.plugins`中的API文档。")
+        ("参见 :py:mod:`aiida.plugins`中的API文档。", "参见 :py:mod:`aiida.plugins`中的API文档。"),
     ]
 )
 def test_str_pp(input, expected):
     """test post process the string for code snippet"""
     got = str_pp(input)
     assert got == expected
+    
+    
+PO_STR = """# SOME DESCRIPTIVE TITLE.
+# Copyright (C) 2014-2020, ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE (Theory and Simulation of Materials (THEOS) and National Centre for Computational Design and Discovery of Novel Materials (NCCR MARVEL)), Switzerland and ROBERT BOSCH LLC, USA. All rights reserved
+# This file is distributed under the same license as the AiiDA package.
+# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
+# 
+#, fuzzy
+msgid ""
+msgstr ""
+"Project-Id-Version: AiiDA 2.0\n"
+"Report-Msgid-Bugs-To: \n"
+"POT-Creation-Date: 2022-11-03 14:17+0000\n"
+"PO-Revision-Date: 2022-11-03 14:19+0000\n"
+"Language-Team: Chinese (China) (https://www.transifex.com/aiidateam/teams/98967/zh_CN/)\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"Language: zh_CN\n"
+"Plural-Forms: nplurals=1; plural=0;\n"
+
+#: ../../source/internals/engine.rst:5
+msgid "Engine"
+msgstr "引擎（已翻译）"
+
+#: ../../source/internals/engine.rst:5
+msgid "Engine"
+msgstr ""
+
+#: ../../source/internals/engine.rst:38
+msgid "The ``WorkflowNode`` example"
+msgstr ""
+
+#: ../../source/internals/engine.rst:19
+msgid ""
+"There are several methods which the internal classes of AiiDA use to control"
+" the caching mechanism:"
+msgstr ""
+"""
+
+def test_po_translate(data_regression):
+    """The actuall process of po file"""
+    lines = PO_STR.splitlines()
+    
+    translated_lines = po_translate(lines)
+    data_regression.check(translated_lines)
+    
