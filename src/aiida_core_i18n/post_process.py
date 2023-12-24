9fbfbafb6379a80e09a852d675a0bc4ecb6cf047
Jusong Yu <jusong.yeu@gmail.com>
2023-12-25 00:56:16 +0100

    New post processing design by squeeze space and add back

--- src/aiida_core_i18n/post_process.py
+++ src/aiida_core_i18n/post_process.py
@@ -1,25 +1,19 @@
-import re
-
-def str_post_processing(raw_str: str) -> str:
+def str_post_processing(tstr: str) -> str:
     """deepl zh_CN has problem when translate the `` in CJK from English,
     this method will handle the process of it to render code snippet.
     """
-    # if `{any_str}' -> ``{any_str}``
-    tstr = re.sub(r"""(?:(?:(?<!`)(?<!:))`(\w.*?)')""", r'``\1``', raw_str, flags=re.ASCII)
+    ## for `{content}` make sure a space in front
+    #tstr = re.sub(r'(?:(?:(?<!^)(?<!\s)(?<!`)(?<!:))(`\w.*?`))', r' \1', tstr, flags=re.ASCII)
     
-    # if \" {any_str} \" -> ``{any_str}``
-    tstr = re.sub(r'(?<!\w)\"(.*?)\"', lambda m: f"``{m.group(1).strip()}``", tstr, flags=re.ASCII)
-    
-    # for `{content}` make sure a space in front
-    tstr = re.sub(r'(?:(?:(?<!^)(?<!\s)(?<!`)(?<!:))(`\w.*?`))', r' \1', tstr, flags=re.ASCII)
-    
-    ## for ``{content}`` make sure a space in front
-    #tstr = re.sub(r'(?:(?:(?<!^)(?<!\s)(?<!`))(``\w.*?``))', r' \1', tstr, flags=re.ASCII)
+    ## r"请访问 ``话语论坛 <https://aiida.discourse.group> `__``。" -> r"请访问 `话语论坛 <https://aiida.discourse.group>`__。"
+    #tstr = re.sub(r"``(.*?)\s+`__``", r"`\1`__ ", tstr, flags=re.ASCII)
 
-    # r"请访问 ``话语论坛 <https://aiida.discourse.group> `__``。" -> r"请访问 `话语论坛 <https://aiida.discourse.group>`__。"
-    tstr = re.sub(r"``(.*?)\s+`__``", r"`\1`__ ", tstr, flags=re.ASCII)
-
-    # Strip the space in both ends, otherwise the next pp will be revert
+    ## Strip the space in both ends, otherwise the next pp will be revert
+    #tstr = tstr.strip()
+    
+    # The post processing squeeze the string to without space
+    # The space is added in the revert_protected
+    tstr = tstr.replace(" ", "")
     tstr = tstr.strip()
 
     return tstr
