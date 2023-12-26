874448781332adf9c829d714d18113ce3da14723
Jusong Yu <jusong.yeu@gmail.com>
2023-12-26 13:37:01 +0100

    Use only ascii brackets

--- src/aiida_core_i18n/post_process.py
+++ src/aiida_core_i18n/post_process.py
@@ -22,8 +22,12 @@ def str_post_processing(raw_str: str) -> str:
     # r"请访问 ``话语论坛 <https://aiida.discourse.group> `__``。" -> r"请访问 `话语论坛 <https://aiida.discourse.group>`__。"
     tstr = re.sub(r"``(.*?)\s+`__``", r"`\1`__ ", tstr, flags=re.ASCII)
 
+    # Use only the ascii bracket
+    tstr = tstr.replace("（", "(")
+    tstr = tstr.replace("）", ")")
+
     # fix issue 102
-    tstr = tstr.replace("``（", "`` （")
+    tstr = tstr.replace("``(", "`` (")
 
     # Make no sence to have anywhere in the string double space, replace it with single space
     tstr = re.sub(r"\s{2,}", " ", tstr, flags=re.ASCII)
